4e146bc321366bb50b9b3f5cdd0fc4a99ee1c9af
busranurbaydur <busranurbaydur@outlook.com>
2022-10-16 14:12:56 +0300

    Add project files.

---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            Context c = new Context();
+          
+                var bloglar = c.blogs.ToList();
+                return View(bloglar);           
+        }
+        public ActionResult BlogDetay (int id)
+        {
+
+            return View();
+
+        }
+    }
+}
---
+++ TravelTripProje/Models/Siniflar/Context.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class Context : DbContext
+    {
+       public DbSet <Admin> admins { get; set; }
+       public DbSet <AdresBlog> adresBlogs { get; set; }
+       public DbSet <Blog> blogs { get; set; }
+       public DbSet <Hakkimizda> hakkimizdas { get; set; }
+       public DbSet <İletisim> iletisims { get; set; }
+       public DbSet <Yorumlar> yorumlars { get; set; }
+       
+    }
+}
